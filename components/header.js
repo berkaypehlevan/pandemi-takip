04f14a9c12eb0489b98361a42b1d4d64679e43a1
Berkay Pehlevan <controloot@protonmail.ch>
2020-05-25 10:23:08 +0300

    Düzenleme

--- components/header.js
+++ components/header.js
@@ -1,7 +1,6 @@
 function Header() {
   return (
     <div>
-<<<<<<< HEAD
       <h1
         id="title"
         className="bg-light element-animated fade-in long  w-50 mx-auto "
@@ -15,22 +14,6 @@ function Header() {
       >
         Covid-19 Türkiye İstatistikleri
       </p>
-=======
-      <h3 id="title" className="element-animated fade-in long mt-5">
-        Pandemi Takip
-      </h3>
-      <div
-        id="kart"
-        className="element-animated fade-in long card mb-3 mt-3 w-50 mw-25 mx-auto"
-      >
-        <div className="card-body">
-          <p className="card-text">
-            Pandemi ile ilgili global istatistiklere güncel olarak
-            erişebilirsiniz.
-          </p>
-        </div>
-      </div>
->>>>>>> dff371398c294d162ebb2a58df5c433bb8ebe3ca
     </div>
   );
 }
